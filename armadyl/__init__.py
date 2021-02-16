02a78c602fb14e612265f00c0e0d41e223869d17
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2021-02-15 22:41:39 -0500

    Small bugfixes, flake8 fixes.

--- armadyl/__init__.py
+++ armadyl/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# flake8: noqa: F401
 """
 Armadyl: a lightweight Python web framework.
 """
@@ -8,4 +9,6 @@ __license__ = 'MIT License'
 __copyright__ = 'Copyright 2021 Taylor Fox Dahlin'
 
 # Imports here will be available via `import Armadyl.<var>`
-from src.version import __version__
+from .version import __version__
+from pycnic.core import WSGI
+from .base import BaseHandler, JsonEndpoint, ServeFile
--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 """This module contains setup instructions for armadyl."""
 import os
+from setuptools import setup
 
 curr_dir = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(curr_dir, 'README.md')) as f:
     long_description = f.read()
 
-with open(os.path.join(curr_dr, 'armadyl', 'version.py')) as f:
+with open(os.path.join(curr_dir, 'armadyl', 'version.py')) as f:
     exec(f.read())
 
 setup(
     name='armadyl',
-    version=__version__,
+    version=__version__,  # noqa: F821
     author='Taylor Fox Dahlin',
     packages=['armadyl'],
-    package_data={'': ['LICENSE'],},
+    package_data={'': ['LICENSE'], },
     url='https://github.com/tfdahlin/armadyl',
     license='MIT',
     entry_points={
